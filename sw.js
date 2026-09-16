ea3ae3a09ca8a91c801c8ba2f0d6f6d68b512220
oracle-master <krutoeimya12345@gmail.com>
2026-09-16 13:59:26 +0400

    Add service worker for asset caching
    Implement service worker for caching assets and handling fetch requests.

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
+// Service Worker для Карты ям Отрадного
+const CACHE_NAME = 'otradny-yamy-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon.svg'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache error:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
